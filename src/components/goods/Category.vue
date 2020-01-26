6d16f5547b82b5dfcf0996a1392854aef5a92e0b
lyjady <lyj2780866735>
2020-01-26 20:25:09 +0800

    更新代码

---
+++ src/components/goods/Category.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card">
+      商品分类
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style scoped>
+  .box-card {
+    margin-top: 20px;
+  }
+</style>
