7654e8af8dd71301469576941837002127174c8b
lyjady <lyj2780866735>
2020-01-27 22:03:03 +0800

    更新代码

--- src/components/goods/Category.vue
+++ src/components/goods/Category.vue
@@ -6,14 +6,55 @@
       <el-breadcrumb-item>商品分类</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card class="box-card">
-      商品分类
+      <el-button type="primary" round>添加分类</el-button>
+      <zk-table :data="categoryList" :columns="columns" :selection-type="false" :expand-type="false" border index-text="#" :show-index="true"></zk-table>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: ''
+  name: '',
+  data () {
+    return {
+      queryInfo: {
+        pageNumber: 1,
+        pageSize: 5
+      },
+      total: 0,
+      categoryList: [],
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'catName'
+        }
+      ]
+    }
+  },
+  methods: {
+    async getCategory () {
+      const { data: response } = await this.$http({
+        url: 'goods/getGoodsCategoryTree',
+        method: 'post',
+        data: this.queryInfo,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      if (response.status === 200) {
+        this.categoryList = response.data
+      }
+    },
+
+    async getCategoryCount () {
+      const { data: response } = await this.$http.get('goods/categoryCount')
+      this.count = response
+    }
+  },
+  created () {
+    this.getCategory()
+    this.getCategoryCount()
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,14 +6,18 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
+import ZkTable from 'vue-table-with-tree-grid'
 
 Vue.config.productionTip = false
+
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8081/shop/'
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
+
+Vue.component(ZkTable.name, ZkTable)
 
 new Vue({
   router,
